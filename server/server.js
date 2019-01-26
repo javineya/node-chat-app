34aa0b627a18ae27c5f3eff92db022c864a39b03
Johnny Vineyard <javineya@gmail.com>
2019-01-26 13:03:54 +0900

    added socketIO

--- server/server.js
+++ server/server.js
@@ -4,12 +4,15 @@
 // C:\Users\javin\Desktop\CODING\01-Courses\04-Udemy\01-CompleteNodeJSDeveloper\node-chat-app\server/../public
 
 
-// a better way of doing it to avoid in-n-out jumps; path is node built-in
+// a better way of doing it to avoid in-n-out jumps; .path is node built-in
 const path = require( 'path' );
+// http is used by express to set up our server; we need it explicitly here
+const http = require( 'http' );
 const publicPath = path.join( __dirname, '../public');
 // console.log( publicPath );
 // RESULT: Notice how it does NOT go into /server at all
 // C:\Users\javin\Desktop\CODING\01-Courses\04-Udemy\01-CompleteNodeJSDeveloper\node-chat-app\public
+const socketIO = require( 'socket.io' );
 
 const port = process.env.PORT || 3000;
 
@@ -18,11 +21,26 @@ const express = require( 'express' );
 
 // give us a shorthand method of using express
 let app = express();
+// use this to allow for socketIO connections to our app
+let server = http.createServer( app );
+// this is to set up our websocket server
+let io = socketIO( server );
 
-// tell express which folder we're using for our app
+// tell express where to find our app
 app.use( express.static( publicPath ));
 
+// create event listeners for our websock server to listen for
+// listen for a new connection to the app (most popular event)
+io.on( 'connection', ( socket ) => {
+    console.log( 'New user connected.' );
+
+    socket.on( 'disconnect', () => {
+      console.log( 'Client disconnected.' );
+    });
+});
+
+
 // tell express to listen for changes on the server port we set up
-app.listen( port, () => {
+server.listen( port, () => {
   console.log( `Server up on port ${ port }.`)
 });
