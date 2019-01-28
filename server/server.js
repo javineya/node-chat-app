98ddf47cdbc97896f72c258f042e85ccccddf54e
Johnny Vineyard <javineya@gmail.com>
2019-01-28 12:43:36 +0900

    working chat

--- server/server.js
+++ server/server.js
@@ -40,11 +40,12 @@ io.on( 'connection', ( socket ) => {
 
     socket.broadcast.emit( 'newMessage', generateMessage( 'Admin', 'New user joined.'));
 
-    socket.on( 'createMessage', ( message ) => {
+    socket.on( 'createMessage', ( message, callback ) => {
       console.log( message );
       // io.emit() emits to every connection logged in
       io.emit( 'newMessage', generateMessage( message.from, message.text ));
 
+      callback( 'Server sayeth thus.' );
     });
 });
 
