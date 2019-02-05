dc0ee89b3b0d62d8421e805653b0c18c1d22353b
Johnny Vineyard <javineya@gmail.com>
2019-02-05 14:04:54 +0900

    privatized rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -75,7 +75,6 @@ jQuery( '#message-form' ).on( 'submit', function( event ) {
   let messageTextBox = jQuery('[name=message]');
 
   socket.emit( 'createMessage', {
-    from: 'User',
     text: messageTextBox.val()
   }, function() {
     // clear message form after message sent
